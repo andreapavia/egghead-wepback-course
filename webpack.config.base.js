fe54b44aae8f50c4a43180c921c7238d8e461a78
Andrea Francesco Pavia <andreafrancesco.pavia@rcww.leoburnett.it>
2019-05-23 11:09:54 +0200

    add babel polyfill

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,7 +14,10 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets:['@babel/preset-env', '@babel/preset-react'],
+                    presets:[
+                        '@babel/preset-env', 
+                        '@babel/preset-react'
+                    ],
                     plugins: [
                         'react-hot-loader/babel',
                         '@babel/plugin-proposal-class-properties'
